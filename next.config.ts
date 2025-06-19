965a650da60b813c2a8cf51d0e15cbb7c2effd78
yntymakdev <kubanychevyntymak21@gmail.com>
2025-06-19 20:40:45 +0600

    starting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    SERVER_URL: process.env.SERVER_URL,
+    APP_URL: process.env.APP_URL,
+  },
 };
 
 export default nextConfig;
